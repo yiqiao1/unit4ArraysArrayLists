4889cfd6fe4fdb436d00bae6b91ea1b49ea8c540
yiqiao1 <yiqiao5785@gmail.com>
2014-11-18 15:09:45 -0600

    chap7 practice

---
+++ Chap7Practice/ArrayOperations.java
@@ -0,0 +1,24 @@
+public class ArrayOperations
+{
+    public static void main(String[] args)
+    {
+        double[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100};
+        
+        System.out.println("Length: " + x.length);
+        System.out.println("First item: " + x[0]);
+        System.out.println("Last item: " + x[8]);
+        System.out.println("x[x.length - 1]: " + x[x.length - 1]);
+        
+        for (int i = 0; i < 9; i++)
+        {
+            System.out.println(x[i]);
+        }
+        
+        for (int i = 0; i < 9; i++)
+        {
+            System.out.println("x[" + i + "]: " + x[i]);
+        }
+        
+        for (int i = 8; 
+    }
+}
---
+++ Chap7Practice/PartiallyFilledArray.java
@@ -0,0 +1,75 @@
+public class PartiallyFilledArray
+{
+    private int [] values;
+    private int currentSize;
+    
+    public PartiallyFilledArray()
+    {
+        this.values = new int[10];
+        this.currentSize = 0;
+    }
+    
+    public String toString()
+    {
+        String str = "[ ";
+        
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            if (i > 0)
+            {
+                str += ", ";
+            }
+            
+            str += this.values[i];
+        }
+        
+        str += " ]";
+        return str;
+    }
+    
+    public void fillNElementsRandomly(int numberOfElements, int maxValue)
+    {
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            this.values[i] =(int)(Math.random() * maxValue);
+            this.currentSize++;
+        }
+    }
+    
+    public void removeElementAtIndex(int index)
+    {
+        this.values[index] = this.values[this.currentSize - 1];
+        this.currentSize --;
+    }
+    
+    public void swapElements(int index1, int index2)
+    {
+        int tempValue = this.values[index1];
+        this.values[index1] = this.values[index2];
+        this.values[index2] = tempValue;
+    }
+    
+    public void insertParameter(int value)
+    {
+        if (this.currentSize == this.values.length)
+        {
+            growArray();
+        }
+        
+        this.values[this.currentSize] = value;
+        currentSize++;
+    }
+    
+    private void growArray()
+    {
+        int newArraySize = this.values.length*2;
+        int[] newArray = new int[newArraySize];
+        
+        for (int i = 0; i < this.values.length; i++)
+        {
+            newArray[i] = this.values[i];
+        }
+        
+        this.values = newArray;
+    }
+}
